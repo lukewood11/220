add3746c12944d7c3cf34a6aa18fac3ff32ec768
lukewood <woodlj@g.cofc.edu>
2021-09-01 11:05:34 -0400

    completed hw1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,3 +1,13 @@
+"""
+Name: Luke Wood
+hello.py
+
+Problem: this program illustrates a hello function
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
 def main():
         print("hello, world!")
 
